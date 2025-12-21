b5a72e716f3cf03239b130f2f874dee59f462dbb
Eika <3r1kas123@gmail.com>
2025-12-21 17:58:33 +0100

    on q. 08

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversed = "";
 
-};
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+    };
+
+    console.log(reverseString("Hello21321,,.21233"));
 
 // Do not edit below this line
 module.exports = reverseString;
