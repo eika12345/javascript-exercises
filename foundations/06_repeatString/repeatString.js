171963cf1fd17216b28fc0b8d463967fba16d74c
Eika <3r1kas123@gmail.com>
2025-12-21 17:23:06 +0100

    on q. 07

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(str, times) {
+    if (times < 0) {
+        return "ERROR";
+    }
+    let result = "";
+    for (let i = 0; i < times; i++) {
+        result += str;
+    }
+     return result;
 
 };
+
+console.log(repeatString("hey", 3))
 
 // Do not edit below this line
 module.exports = repeatString;
