e605ce7d1001eb36a46f4060a29f58ea8a07f40f
Eika <3r1kas123@gmail.com>
2025-12-23 17:33:12 +0100

    on q.09

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,37 @@
-const removeFromArray = function() {
-};
+
+//const removeFromArray = function(array, ...valueToRemove) {
+//   for (let i = 0; i < valueToRemove.length; i++) {
+//   let value = valueToRemove[i];
+   
+
+//   let index = array.indexOf(value);
+
+//   for (each value in valueToRemove) {
+//   if (index !== -1) {
+//    array.splice(index, 1);
+
+//    return array
+//   }
+//   }
+//   }
+//};
+
+const removeFromArray = function(array, ...valueToRemove) { // array parameters ([1,2,3,4], 2 ,3) etc
+
+    for (let i = 0; i < valueToRemove.length; i++) {
+         let value = valueToRemove[i]; // goes through each value in valueToRemove to find what value to remove (2,3) 
+    
+    while (array.indexOf(value) !== -1) {
+        let index = array.indexOf(value);   
+        array.splice(index, 1);
+    }
+}
+
+     return array;
+
+}
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
