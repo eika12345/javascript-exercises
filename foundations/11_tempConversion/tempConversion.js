9a2359c4806ce5f2bdab4f96925086c7c5549d26
Eika <3r1kas123@gmail.com>
2025-12-28 16:36:44 +0100

    on q.12

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  var celcius = (temperature - 32) * 5/9;
+  celcius = Math.round(celcius * 10) / 10; 
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  var fahrenheit = (temperature * 9/5) + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10; 
+  return fahrenheit;
 };
 
 // Do not edit below this line
